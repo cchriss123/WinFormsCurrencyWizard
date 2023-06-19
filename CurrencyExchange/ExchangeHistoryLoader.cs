d450e6a6d9445094b938e974cb53880bbb2c73c1
cchriss123 <114023933+cchriss123@users.noreply.github.com>
2023-06-19 23:54:21 +0200

    adds file loader

---
+++ CurrencyExchange/ExchangeHistoryLoader.cs
@@ -0,0 +1,17 @@
+﻿namespace CurrencyExchangeConsole
+{
+    internal class ExchangeHistoryLoader
+    {
+        public static void LoadExchanges(List<CurrencyExchange> currencyExchanges, string filePath)
+        {
+            if (!File.Exists(filePath))
+                return;
+            
+            currencyExchanges.AddRange(
+                File.ReadAllLines(filePath)
+                    .Select(line => line.Split(','))
+                    .Where(values => values.Length >= 5)
+                    .Select(values => new CurrencyExchange(values[0], values[1], double.Parse(values[2]), double.Parse(values[3]), double.Parse(values[4]))));
+        }
+    }
+}
