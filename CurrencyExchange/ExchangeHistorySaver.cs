90df6fc9635f483b9788b7a4ed47f0c0f8fb101f
cchriss123 <114023933+cchriss123@users.noreply.github.com>
2023-06-19 23:51:45 +0200

    adds save function

---
+++ CurrencyExchange/ExchangeHistorySaver.cs
@@ -0,0 +1,12 @@
+﻿namespace CurrencyExchangeConsole
+{
+    internal class ExchangeHistorySaver
+    {
+        public static void SaveExchanges(List<CurrencyExchange> currencyExchanges, string filePath)
+        {
+            using StreamWriter writer = new(filePath);
+            foreach (CurrencyExchange exchange in currencyExchanges) 
+               writer.WriteLine($"{exchange.FromCode},{exchange.ToCode},{exchange.Price},{exchange.AmountPaid},{exchange.AmountReceived}");            
+        }
+    }
+}
