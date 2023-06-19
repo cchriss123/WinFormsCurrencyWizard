4cbe58725924f8b7907902b28c1e0c6cff0033d9
cchriss123 <114023933+cchriss123@users.noreply.github.com>
2023-06-19 23:43:27 +0200

    Adds exchangerateprovider

---
+++ CurrencyExchange/ExchangeRateProvider.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+namespace CurrencyExchangeConsole
+{
+    public class ExchangeRateProvider
+    {
+
+        public static double GetExchangeRate(string fromCode, string toCode)
+        {
+            string apiKey = "nk74nn32lem10m8cl4tr808i3s7mm5ro35566liuo28ulltbn0b19";
+            string urlString = "https://anyapi.io/api/v1/exchange/rates?base=" + fromCode.ToUpper() + "&apiKey=" + apiKey;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(urlString).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                JObject json = JObject.Parse(result);
+                double rate = (double)json["rates"][toCode];
+                // Console.WriteLine(json.ToString());
+                return rate;
+            }
+            else
+            {
+                Console.WriteLine("Error: Unable to fetch data from the API. Response code: " + response.StatusCode);
+                return 0;
+            }
+        }
+
+    }
+}
